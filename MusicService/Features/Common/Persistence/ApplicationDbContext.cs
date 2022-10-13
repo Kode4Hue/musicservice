c488f97f3f95e8d7c0fb17e9601ba0a90dc5211f
kodehue <hubertgraham@outlook.com>
2022-10-13 07:44:55 -0500

    Added initial persistence db context for Music Service/Api

--- MusicService/Features/Common/Persistence/ApplicationDbContext.cs
+++ MusicService/Features/Common/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace MusicService.Features.Common.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using MusicService.Features.Artists.Domain.Entities;
+using MusicService.Features.Music.Domain.Entities;
+
+namespace MusicService.Features.Common.Persistence
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext: DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Song> Songs { get; set; }
     }
 }
