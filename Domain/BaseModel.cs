49cfb4c9a46bf1045b3b82e79cf485e0d2112517
kodehue <hubertgraham@outlook.com>
2022-10-14 00:28:39 -0500

    Removed uneccessary Domain, Infrastructure and Application projects

--- Domain/BaseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain
-{
-    public abstract class BaseModel
-    {
-        public long Id { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime LastModified { get; set; }
-    }
-}
